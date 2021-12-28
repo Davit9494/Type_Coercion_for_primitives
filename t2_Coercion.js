46e81ff972e08cda709c56d51162026e1b5ae3e2
Davit9494 <davitsahakyan94@mail.ru>
2021-12-28 21:54:12 +0400

    fix: Fixed some bugs

--- t2_Coercion.js
+++ t2_Coercion.js
@@ -12,24 +12,26 @@ isNaN(" "); // =false
 9007199254740992 + 1 == 9007199254740992; // false // 2**53 is not safe integer
 9007199254740992 + 2 == 9007199254740992; // false,
 
-(0 ===
-  +0 + // true, 0 is equal to -0, they show difference only in Object.is() and in division with infinities
-    0) ===
-  -0; // true
+
+0 === +0 // true, 0 is equal to -0, they show difference only in Object.is() and in division with infinities
++0 === -0  // true
+
 
 1 / "2"; // 0.5, division is not defined for strings
-1 / 0; // Infinity
-1 / -0; // -Infinity
+1 / 0; // Infinity  
+1 / -0; // -Infinity  
 
 Infinity / 6; // infinity, infinity is positive num, + / + is + number
 Infinity / -256; // -infinity, + / - is - number
 Infinity / Infinity;
 
-0 / 0 + // NaN
-  true + // 1 true as a number is 1
-  false + // 0, false as a number is 0
-  null + // 0, null as a number is 0
-  undefined; // undefined and NaN as a number are NaN
+
+0 / 0  // NaN  
++true  // 1 true as a number is 1
++false  // 0, false as a number is 0
++null   // 0, null as a number is 0
++undefined // undefined and NaN as a number are NaN
+
 
 parseInt(".2");
 console.log(parseInt("077a")); // 77
